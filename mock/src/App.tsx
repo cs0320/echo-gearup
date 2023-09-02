97ae9f9d3966e6dac764569ccdbc6a94eba932bb
Tim Nelson <tn@cs.brown.edu>
2023-09-02 10:02:47 -0400

    feat: reactify

---
+++ mock/src/App.tsx
@@ -0,0 +1,15 @@
+import REPL from './REPL'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="App-header">
+        React Example
+      </p>
+      <REPL />      
+    </div>
+  );
+}
+
+export default App;
