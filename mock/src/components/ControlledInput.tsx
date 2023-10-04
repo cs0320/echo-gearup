0eefcfc733a58da74c37d66a04ce56d5830e0c7d
jmaffa <90526195+jmaffa@users.noreply.github.com>
2023-10-04 13:06:36 -0400

    chore: added HTML changes to React

--- mock/src/components/ControlledInput.tsx
+++ mock/src/components/ControlledInput.tsx
@@ -17,6 +17,7 @@ interface ControlledInputProps {
     return (
       <input type="text" className="repl-command-box"
             value={value} 
+            placeholder="Enter command here!"
             onChange={(ev) => setValue(ev.target.value)}
             aria-label={ariaLabel}>
       </input>
